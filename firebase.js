c594930fb489ab2735ba48edabd9e995afeb81ba
siddapichau <161685092+siddapichau@users.noreply.github.com>
2026-07-11 18:56:32 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,12 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+'use strict';
+console.log('firebase.js carregado (Roda dos Drinks)');
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
+window.firebaseConfig = {
   apiKey: "AIzaSyDC6PDG-id7qrDEN37DJ32n0ew6HBhiIBg",
   authDomain: "rodadodrinks.firebaseapp.com",
+  databaseURL: "https://rodadodrinks-default-rtdb.firebaseio.com",
   projectId: "rodadodrinks",
   storageBucket: "rodadodrinks.firebasestorage.app",
   messagingSenderId: "552484960442",
   appId: "1:552484960442:web:fdf816e4b1d8c64cd26a83"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-// Exporte o app se for utilizá-lo em outros arquivos JS
-export { app };
