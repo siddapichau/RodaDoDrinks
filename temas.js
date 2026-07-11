8c75b62e8d17ddee5e8f0c170c130a263dcd8825
siddapichau <161685092+siddapichau@users.noreply.github.com>
2026-07-11 18:31:24 -0300

    Create temas.js

---
+++ temas.js
@@ -0,0 +1,14 @@
+export const temas = {
+    padrao: {
+        background: "#1a0b2e",
+        texto: "#ffffff",
+        roletaCores: [
+            "#FF0055", // Rosa neon
+            "#00F0FF", // Azul cyan
+            "#00FF66", // Verde neon
+            "#FFD700", // Dourado
+            "#FF8C00", // Laranja
+            "#9400D3"  // Roxo escuro
+        ]
+    }
+};
