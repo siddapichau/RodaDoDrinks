eed7bd7ee03e6a99cf5232fbd8e3d500859b9483
siddapichau <161685092+siddapichau@users.noreply.github.com>
2026-07-11 19:06:04 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,57 +1,26 @@
 const CACHE_NAME = 'roda-drinks-v1';
 const urlsToCache = [
-    './',
-    './index.html',
-    './crono.html',
-    './receita.html',
-    './admin-secreto.html',
-    './style.css',
-    './responsivo.css',
-    './app.js',
-    './core.js',
-    './efeitos.js',
-    './firebase.js',
-    './roleta.js',
-    './temas.js',
-    './logo.png',
-    './resources/spin.mp3',
-    './resources/win.mp3'
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/core.js',
+  '/logo.png',
+  '/manifest.json',
+  '/efeitos.js',
+  '/roleta.js',
 ];
 
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                console.log('Cache aberto - Arquivos salvos para offline');
-                return cache.addAll(urlsToCache);
-            })
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                // Retorna o cache se existir, senão faz a requisição na rede
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
-            })
-    );
-});
-
-self.addEventListener('activate', event => {
-    const cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
